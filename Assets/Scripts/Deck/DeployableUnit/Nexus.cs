62f01668d2b1726584e89154f9e07b5d27e1dc32
Adi Zhavo <adi@gram.gs>
2016-06-19 15:00:43 +0300

    Added nexus class

---
+++ Assets/Scripts/Deck/DeployableUnit/Nexus.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nexus : Tower {
+
+    #region Deployable Implementation
+
+    public override void Destroy()
+    {
+        base.Destroy();
+    }
+    #endregion
+}
