1ce12a9a2491d49eb94251aacafaf3f9df52ca38
Adi Zhavo <adi@gram.gs>
2016-06-19 12:51:20 +0300

    Added Hp UI bard for each destroyable

--- Assets/Scripts/Deck/DeployableUnit/SoldierHPBar.cs
+++ Assets/Scripts/Deck/DeployableUnit/SoldierHPBar.cs
@@ -6,8 +6,22 @@ public class SoldierHPBar : MonoBehaviour
     [SerializeField] private int maxHp;
     public int MaxHp { get { return maxHp; } }
 
+    [SerializeField] private Transform CurrentHp;
+    [SerializeField] private Transform Damage;
+
+    private Damagable unitDamage;
+
     public void Init(Damagable unitDamage)
     {
-        // UI Health bar
+        this.unitDamage = unitDamage;
+    }
+
+    private void Update()
+    {
+        float xScale = unitDamage.GetHp() / maxHp;
+        xScale = Mathf.Clamp01(xScale);
+
+        CurrentHp.localScale = new Vector3(xScale, 1f, 1f);
+        Damage.localScale = Vector3.Lerp(Damage.localScale, CurrentHp.localScale, Time.deltaTime * 3);
     }
 }
